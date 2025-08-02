f5aff6a2998734fcfaa4aeba44f43c3e2c70e01f
User <User@MacBook-Pro.local>
2025-08-02 09:22:18 +0400

    add main block

--- index.js
+++ index.js
@@ -7,17 +7,21 @@ burgerMenu()
 const modal = document.querySelector('.overlay')
 const openModalBtn = document.querySelector('#open-modal')
 const closeModalBtn = document.querySelector('#modal-close')
+modal.classList.add('noActive')
 
 openModalBtn.addEventListener('click', () => {
+  modal.classList.remove('noActive')
   modal.classList.add('active')
 })
 
 closeModalBtn.addEventListener('click', () => {
   modal.classList.remove('active')
+  modal.classList.add('noActive')
 })
 
 modal.addEventListener('click', (event) => {
   if (event.target === modal) {
     modal.classList.remove('active')
+    modal.classList.add('noActive')
   }
 })
